4041aed56e12d82194c46441cdc73881d0f88dc6
unknown <parkerosemeyer@gmail.com>
2024-05-08 13:52:36 -0500

    Added prompts and plugins for color and input limit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-var inquirer = require('inquirer')
+const inquirer = require('inquirer')
+const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
+const Color = require('color')
+
+inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
+
+inquirer
+    .prompt([
+        {
+            type: 'maxlength-input',
+            name: 'text',
+            message: 'Enter up to three characters',
+            maxLength: 3
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter a color for text'
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Pick a shape',
+            choices: ['square', 'circle', 'triangle']
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter a color for the shape'
+        }
+    ])
